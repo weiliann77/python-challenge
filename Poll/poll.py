ec067638b6f2904beed4df01de7ceb23aae9a3e1
weiliann77 <64275919+weiliann77@users.noreply.github.com>
2020-05-28 23:34:27 -0500

    Update poll.py

--- Poll/poll.py
+++ Poll/poll.py
@@ -1,109 +1,95 @@
 # -*- coding: utf-8 -*-
 """
-Created on Thu May 28 13:42:55 2020
+Created on Thu May 28 21:37:22 2020
 
 @author: Brodie
 """
 
-
-# -*- coding: utf-8 -*-
-"""
-Created on Mon May 25 14:00:00 2020
-
-@author: Brodie
-"""
-# First we'll import the os module
-# This will allow us to create file paths across operating systems
 import os
-# import statistics
-import pandas as pd
+
+
 # Module for reading CSV files
-# import csv
+import csv
 
+count = 0
+candidates = []
+indexer = []
+indexed = []
+final = []
+# csvpath = os.path.join('..', 'Poll','Resources', 'election_data.csv')
 csvpath = os.path.join('..', 'Poll','Resources', 'election_data.csv')
-df= pd.read_csv(csvpath, encoding="ISO-8859-1")
-# data1 = df.head()
-# print(data1)
-# print(df.head())
-# print(df.sum())
-df1 = df.groupby("Candidate")
-df6 = df1["Voter ID"].count().rename_axis('Candidate').reset_index(name='Votes')
-df2 = df1["Voter ID"].count()
-df3 = len(df)
-# print(df2)
-print('Total Votes:', df3)
-df4 = pd.DataFrame((df2/df3))
-df5 = df4.rename(columns={'Voter ID':'Votes'})
-df7 = df5.merge(df6, on="Candidate")
-df8 = df7.rename(columns={'Votes_x':'Vote Percentage','Votes_y':'Votes'})
-print(df8)
-# print(df8['Votes'].max())
-val = df8['Votes'].max()
-print("winner: " , df8.loc[df8['Votes']==val,'Candidate'].item())
+with open(csvpath) as csvfile:
 
+    # CSV reader specifies delimiter and variable that holds contents
+    csvreader = csv.reader(csvfile, delimiter=',')
+
+    # print(csvreader)
+
+    # Read the header row first (skip this step if there is now header)
+    csv_header = next(csvreader)
+    # print(f"CSV Header: {csv_header}")
+
+    # Read each row of data after the header
+    # sum_row = 0
+    # last_row = 0
+    # max_row =0
+    # min_row =0
+    # change=[]
+    counter =0
+    index=0
+    for row in csvreader:
+        counter = counter +1
+        if row[2] not in candidates:
+            candidates.append(row[2])
+            index = index +1
+            indexer.append(index)
+            indexed.append(index)
+with open(csvpath) as csvfile:   
+    csvreader = csv.reader(csvfile, delimiter=',')
+
+    # print(csvreader)
+
+    # Read the header row first (skip this step if there is now header)
+    csv_header = next(csvreader)
+    for row in csvreader:
+        # print(row)          
+        for x in candidates:
+            if row[2] == x:
+              indexer[candidates.index(x)]=indexer[candidates.index(x)]+1
+                
+            
+# print(counter)
+# print(candidates)
+# print(indexer)
 txtpath = os.path.join('..', 'Poll','analysis', 'output.txt')
-file1 = open(txtpath,"w")#write mode 
-file1.write("Election Resuslts \n \n")
-file1.write('total votes ')
-file1.write(str(df3))
-file1.write('\n')
-file1.write('\n')
-file1.write(str(df8))
-file1.write('\n')
-file1.write('\n')
-file1.write("winner:  ")
-file1.write(df8.loc[df8['Votes']==val,'Candidate'].item())
-file1.close() 
-# print(df3.dtypes)
-# print(df2['Votes'].dtypes)
+file1 = open(txtpath,"w")#write mode               
+print("Election Results")
+file1.write("Election Results \n\n")
+file1.write("Total Votes: ")
+file1.write(str(counter))
+file1.write("\n\n")
 
+print("Total Votes: ", counter)
 
-# print(df2.iloc[0:0,0:0])
-
-# print(df1.sum())
-
-# Method 1: Plain Reading of CSV files
-# with open(csvpath, 'r') as file_handler:
-#     lines = file_handler.read()
-#     print(lines)
-#     print(type(lines))
-
-
-# Method 2: Improved Reading using CSV module
-
-# with open(csvpath) as csvfile:
-
-#     # CSV reader specifies delimiter and variable that holds contents
-#     csvreader = csv.reader(csvfile, delimiter=',')
-
-#     print(csvreader)
-
-#     # Read the header row first (skip this step if there is now header)
-#     csv_header = next(csvreader)
-#     print(f"CSV Header: {csv_header}")
-
-#     # # Read each row of data after the header
-#     # sum_row = 0
-#     # last_row = 0
-#     # max_row =0
-#     # min_row =0
-#     # change=[]
-#     # counter =0
+for a in range(len(candidates)):
     
-#     for row in csvreader:
-#     #     sum_row = int(row[1])+sum_row
-#     #     if (int(row[1])-last_row) > max_row:
-#     #         max_row=int(row[1])-last_row
-#     #     if (int(row[1])-last_row) < min_row:
-#     #         min_row=int(row[1])-last_row
-#     #     if counter != 0:
-#     #         change.append((int(row[1])-last_row))
-#     #     counter = counter +1
-#     #     last_row = int(row[1])
-#         print(row)
-#     # x = statistics.mean(change)
-#     # print(sum_row)
-#     # print(max_row)
-#     # print(min_row)
-#     # # print(x)
-#     # print(counter)
+    file1.write(candidates[a])
+    file1.write("  ")
+    file1.write(str(round(((indexer[a]-indexed[a])/counter)*100,2)))
+    file1.write("%  ")
+    file1.write(str(indexer[a]-indexed[a]))
+    file1.write("\n")
+    print(candidates[a], round(((indexer[a]-indexed[a])/counter)*100,2),"%",  indexer[a]-indexed[a])
+    final.append(indexer[a]-indexed[a])
+    # print(indexer[a]-indexed[a])
+final2 = max(final)
+# print(final2)
+
+for a in range(len(candidates)):
+    if final[a] == final2:
+        print("Winner:", candidates[a])
+        file1.write("\n Winner:")
+        file1.write(candidates[a])
+
+      
+
